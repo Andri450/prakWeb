0674b34aaa86d4b603e367cf48ea5a1a426a51a5
Andri450 <51738810+Andri450@users.noreply.github.com>
2020-07-08 23:05:22 +0700

    Add files via upload

---
+++ portofolio2/view/home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="view/asset/css/bootstrap.min.css">
+        <link rel="stylesheet" href="view/asset/css/gaya.css">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <script src="view/asset/js/jquery.min.js"></script>
+        
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script> -->
+        <script src="view/asset/js/bootstrap.min.js"></script>
+
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="w-gelap">
+        
+        <!-- navbar -->
+        <?php include("nav.php"); ?>
+        <!-- /navbar -->
+
+        <div class="container-fluid">
+            <div class="box-tulisan col-6">
+                <div style="color: white;">
+                    <p>Hi, My name is</p>
+                    <h1>Andri Setiawan</h1>
+                    <h3>I am fresh graduate a vocational school.</h3>
+                    <h4>This is my portfolio website.</h4>
+                    <p></p>
+                    <hr>
+                    <label class="note"></label>
+                </div>
+            </div>
+            <div class="">
+                <img class="gunung img-fluid" src="view/asset/foto/gunung.png">
+                <img class="awan img-fluid" src="view/asset/foto/awan3.png">
+            </div>
+        </div>
+
+        <div class="saklar">
+
+            <p class="mode" style="display: none;">Terang</p>
+
+            <div class="bulan"><img src="view/asset/foto/bulan3.png"/></div>
+
+            <div class="matahari"><img src="view/asset/foto/matahari.png"/></div>
+
+        </div>
+
+        <div class="container-fluid p-3 mb-5">
+            <div class="container badan w-gelap p-3 mb-5">
+                <div class="m1 col-md-12">
+                        <div class="row">
+                            <div class="col-4 bimg">
+                                <center>
+                                    <img class="fot img-fluid img" src="view/asset/foto/_MG_0017crp6.png">
+                                </center>
+                            </div>
+                            <hr>
+                            <div class="col w-terang ttg">
+                                <h5 class="text-center">About Me.</h5>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic iste blanditiis consequuntur amet aliquam error maiores neque ratione vitae iure!.</p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+        <script src="view/app.js"></script>
+        <script src="view/asset/js/velocity.js"></script>
+           
+    </body>
+</html>
